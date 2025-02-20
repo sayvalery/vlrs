ddeb28afd08037114e851b157f249ef9cab09019
V <pantikvrode@ya.ru>
2025-02-20 04:46:49 +0300

    try 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   site: "https://sayvalery.github.io",
-  base: "/vlrs",
+  base: "/",
   integrations: [mdx(), sitemap(), tailwind()],
 });
